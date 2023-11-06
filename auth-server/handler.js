355df6a5c9856cf1c30d979c377ee262ca96d9b3
bondcab <136003551+bondcab@users.noreply.github.com>
2023-11-06 19:42:28 +0000

    exercise_4.3

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -95,6 +95,8 @@ module.exports.getCalendarEvents = async (event) => {
         statusCode: 200,
         headers: {
           "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Headers": "Content-Type",
+          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
           "Access-Control-Allow-Credentials": true,
         },
 
