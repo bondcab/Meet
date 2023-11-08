1399a65a118de8282b10c029155198d2c3137b6c
bondcab <136003551+bondcab@users.noreply.github.com>
2023-11-08 19:38:01 +0000

    feature 1 unit tests

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import EventList from "./components/EventList";
+import CitySearch from "./components/CitySearch";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <CitySearch />
+      <EventList />
+    </div>
+  );
 }
 
 export default App;
