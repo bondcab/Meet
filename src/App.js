f97901664ac561d027037499f3ecc1e23c484fe5
bondcab <136003551+bondcab@users.noreply.github.com>
2023-11-09 20:49:14 +0000

    App component add EventCount

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import EventList from "./components/EventList";
 import CitySearch from "./components/CitySearch";
+import EventCount from "./components/EventCount";
 
 function App() {
   return (
     <div className="App">
       <CitySearch />
       <EventList />
+      <EventCount />
     </div>
   );
 }
