b9dfd8f9d5bbd21aea31ddca9a6d53c106f58b9d
bondcab <136003551+bondcab@users.noreply.github.com>
2023-11-12 10:45:39 +0000

    integration test - render 32 events on app mount

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import "./App.css";
 import EventList from "./components/EventList";
 import CitySearch from "./components/CitySearch";
 import EventCount from "./components/EventCount";
+import { useEffect, useState } from "react";
+import { getEvents } from "./api";
 
 function App() {
+  const [events, setEvents] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState(32);
+
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    setEvents(allEvents.slice(0, currentNOE));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <CitySearch />
-      <EventList />
+      <EventList events={events} />
       <EventCount />
     </div>
   );
