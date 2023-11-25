67c50a82cf5eb53101af4158eaad0bc4bddddb36
bondcab <136003551+bondcab@users.noreply.github.com>
2023-11-25 11:28:40 +0000

    create warning alert

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import CitySearch from "./components/CitySearch";
 import EventCount from "./components/EventCount";
 import { useEffect, useState } from "react";
 import { getEvents, extractLocations } from "./api";
-import { InfoAlert, ErrorAlert } from "./components/Alert";
+import { InfoAlert, ErrorAlert, WarningAlert } from "./components/Alert";
 
 const App = () => {
   const [events, setEvents] = useState([]);
@@ -13,8 +13,19 @@ const App = () => {
   const [currentCity, setCurrentCity] = useState("See all cities");
   const [infoAlert, setInfoAlert] = useState("");
   const [errorAlert, setErrorAlert] = useState("");
+  const [warningAlert, setWarningAlert] = useState("");
+  let warningText;
 
   useEffect(() => {
+    if (navigator.onLine) {
+      warningText = "";
+      setWarningAlert(warningText);
+    } else {
+      warningText =
+        "You are offline. Events data will be limited to local cache";
+      setWarningAlert(warningText);
+    }
+
     fetchData();
   }, [currentCity, currentNOE]);
 
@@ -33,6 +44,7 @@ const App = () => {
       <div className="alerts-container">
         {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
         {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
+        {warningAlert.length ? <WarningAlert text={warningAlert} /> : null}
       </div>
       <CitySearch
         allLocations={allLocations}
